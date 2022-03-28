73da699d067b2c9ed3bc701b53b684258c55cdc5
knknWEB <knkn.web@gmail.com>
2022-03-28 18:56:27 +0200

    obiektowosc

---
+++ cwiczenie5_KacperNowak/config.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'config.class.php';
+
+$conf = new Config();
+$conf->root_path = dirname(__FILE__);
+$conf->server_name = 'localhost:80';
+$conf->server_url = 'http://'.$conf->server_name;
+$conf->app_root = '/cwiczenie5_KacperNowak';
+$conf->app_url = $conf->server_url.$conf->app_root;
